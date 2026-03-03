19dad72ac1d0662358e6a4d876d2963ffc2fae10
degbaisrael45-afk <degbaisrael45@gmail.com>
2026-03-03 23:21:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
 
+TOKEN = os.environ.get("BOT_TOKEN")
+TELEGRAM_URL = f"https://api.telegram.org/bot{TOKEN}"
+
 @app.route("/")
 def home():
-    return "Football Bot Actif ⚽🔥"
+    return "Bot Telegram Actif ⚽🔥"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json()
+
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+
+        reply = "⚽ Football Bot prêt !"
+
+        if text.lower() == "/start":
+            reply = "Bienvenue sur Football Bot Pro ⚽🔥"
+
+        send_message(chat_id, reply)
+
+    return "ok"
+
+def send_message(chat_id, text):
+    url = f"{TELEGRAM_URL}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": text
+    }
+    requests.post(url, json=payload)
 
 if __name__ == "__main__":
     app.run()
